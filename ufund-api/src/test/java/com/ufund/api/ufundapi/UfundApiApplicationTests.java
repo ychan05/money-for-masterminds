d52d03c155316da1e13996635169f832921bc6a2
Graden <gr4d3n01s0n@gmail.com>
2024-02-19 19:29:27 -0500

    minor changes to applicationTests

--- ufund-api/src/test/java/com/ufund/api/ufundapi/UfundApiApplicationTests.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/UfundApiApplicationTests.java
@@ -1,8 +1,10 @@
 package com.ufund.api.ufundapi;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Tag("Controller-tier")
 @SpringBootTest
 class UfundApiApplicationTests {
 
