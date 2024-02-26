bdbea3f354fb2c3a5a34949de168392a9a20bc8c
ychan05 <yatlongc2005@gmail.com>
2024-02-26 10:28:33 -0500

    init cupboard component

---
+++ ufund-ui/ufund/src/app/cupboard/cupboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cupboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './cupboard.component.html',
+  styleUrl: './cupboard.component.css'
+})
+export class CupboardComponent {
+
+}
